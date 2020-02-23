308a09ebcb0ce801b4d7cc6f5fced2f785ef3c0d
etrzupek23 <57397879+etrzupek23@users.noreply.github.com>
2020-02-23 09:09:24 -0500

    Add files via upload

---
+++ Playlist.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class Playlist {
+
+  public static void main(String[] args) {
+    //new playlist
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+    //adding songs
+    desertIslandPlaylist.add("six feet under");
+    desertIslandPlaylist.add("bury a friend");
+    desertIslandPlaylist.add("ocean eyes");
+    desertIslandPlaylist.add("come out and play");
+    desertIslandPlaylist.add("lovely");
+    desertIslandPlaylist.add("I Lost a Friend");
+    desertIslandPlaylist.add("Wonderwall");
+    desertIslandPlaylist.add("You Will Be Found");
+    //printing out list
+    System.out.println(desertIslandPlaylist);
+    //getting rid of songs
+    desertIslandPlaylist.remove(1);
+    desertIslandPlaylist.remove(1);
+    desertIslandPlaylist.remove(3);
+    //printing out list
+    System.out.println(desertIslandPlaylist);
+    //swapping songs
+    int indexA = desertIslandPlaylist.indexOf("lovely");
+    int indexB = desertIslandPlaylist.indexOf("Wonderwall");
+    String tempA = "lovely";
+    desertIslandPlaylist.set(indexA, "Wonderwall");
+    desertIslandPlaylist.set(indexB, tempA);
+    System.out.println(desertIslandPlaylist);
+
+  }
+
+}
