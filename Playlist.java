275e1232542eb503337b62a76e1adbf9025fc76c
etrzupek23 <57397879+etrzupek23@users.noreply.github.com>
2020-02-24 14:14:49 -0500

    Add files via upload

---
+++ Playlist.java
@@ -0,0 +1,40 @@
+/*
+This program represents a playlist that someone is given on a desert island that adds, removes, and shuffles songs
+Created by Emma on Feb. 23th, 2020
+Intermediate Programming - Block 8
+*/
+import java.util.ArrayList;
+
+class Playlist {
+
+  public static void main(String[] args) {
+    //new playlist
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+    //adding songs
+    desertIslandPlaylist.add("six feet under");
+    desertIslandPlaylist.add("bury a friend");
+    desertIslandPlaylist.add("ocean eyes");
+    desertIslandPlaylist.add("come out and play");
+    desertIslandPlaylist.add("lovely");
+    desertIslandPlaylist.add("I Lost a Friend");
+    desertIslandPlaylist.add("Wonderwall");
+    desertIslandPlaylist.add("You Will Be Found");
+    //printing out list
+    System.out.println(desertIslandPlaylist);
+    //getting rid of songs
+    desertIslandPlaylist.remove(1);
+    desertIslandPlaylist.remove(1);
+    desertIslandPlaylist.remove(3);
+    //printing out list
+    System.out.println(desertIslandPlaylist);
+    //swapping songs
+    int indexA = desertIslandPlaylist.indexOf("lovely");
+    int indexB = desertIslandPlaylist.indexOf("Wonderwall");
+    String tempA = "lovely";
+    desertIslandPlaylist.set(indexA, "Wonderwall");
+    desertIslandPlaylist.set(indexB, tempA);
+    System.out.println(desertIslandPlaylist);
+
+  }
+
+}
